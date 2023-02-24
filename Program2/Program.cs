3861aa10fee77428728be83d0353884ead0d85e6
«Artur <y.artur2008@gmail.com>
2023-02-24 17:08:16 +0300

    Добавили метод вычисления элемента массива, длина которого меньше либо равна 3 и копирования его в другой массив

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -1,3 +1,17 @@
 ﻿string[] text = {"Привет", "world", "2", ":-)"};
 string[] result = new string[text.Length];
 
+void Replace(string[] text, string[] result)
+{
+    int count = 0;
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= 3) 
+        {
+            result[count] = text[i];
+            count++;
+        } 
+    }
+}
+
