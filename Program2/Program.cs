fc52378cadede6f842df87d7a7d01f0f05a2aca8
«Artur <y.artur2008@gmail.com>
2023-02-24 17:08:54 +0300

    Добавили метод вывода масиива на экран

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -15,3 +15,15 @@ void Replace(string[] text, string[] result)
     }
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+Replace(text, result);
+PrintArray(result);
