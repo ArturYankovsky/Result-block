68407d24674dddfddefa2447b24a862c4a3006bd
«Artur <y.artur2008@gmail.com>
2023-02-24 17:29:41 +0300

    Другое решение задачи

---
+++ Program/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] text = {"Привет", "wor", "2", ":-)"};
+string Replace(string[] text)
+{
+    string result = String.Empty;
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i].Length <= 3) result = result + text[i] + " ";
+        
+    }
+    Console.Write(result);
+    return result;
+    
+}
+Console.Clear();
+Replace(text);
+Console.WriteLine();
